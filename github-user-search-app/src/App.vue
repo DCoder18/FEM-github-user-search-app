d60133da1713ea434bf49bc082c2fd0a656fbf8f
Devika <devikasujith12@gmail.com>
2022-05-20 18:11:23 +0400

    Search Section

--- github-user-search-app/src/App.vue
+++ github-user-search-app/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <HeaderSection></HeaderSection>
+    <SearchBar></SearchBar>
   </div>
 </template>
 
 <script>
 import HeaderSection from './components/HeaderSection.vue'
+import SearchBar from './components/SearchBar.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderSection
+    HeaderSection,
+    SearchBar,
   }
 }
 </script>
---
+++ github-user-search-app/src/components/SearchBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <section class="search">
+      <form action="#" method="post">
+        <div class="searchbar">
+          <input type="search" name="" id="" placeholder="Search GitHub username...">
+          <img class="icon icon--search" src="../assets/images/icon-search.svg" alt="">
+          <button class="btn-search">Search</button>
+        </div>
+      </form>
+    </section>
+  </main>
+</template>
+
+
+<style lang="scss" scoped>
+  .searchbar {
+    position: relative;
+  }
+
+  input {
+    width: 100%;
+    height: 7rem;
+    border: none;
+    outline: none;
+    border-radius: 1.5rem;
+    box-shadow: 0px 6px 20px -15px var(--box-shadow-blue);
+    position: absolute;
+    font-size: var(--ff-400);
+    font-family: inherit;
+    padding-left: 8rem;
+  }
+
+  .icon {
+    &--search {
+      width: 2.4rem;
+      height: 2.4rem;
+      position: absolute;
+      top: 2.2rem;
+      left: 2.2rem;
+    }
+  }
+
+  .btn-search {
+    border: none;
+    background-color: var(--clr-bright-blue);
+    color: var(--clr-white);
+    border-radius: 1rem;
+    width: 10rem;
+    height: 5rem;
+    font-family: inherit;
+    font-size: var(--ff-300);
+    position: absolute;
+    right: 1rem;
+    top: 1rem
+  }
+</style>
