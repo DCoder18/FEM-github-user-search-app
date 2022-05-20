835a37ec2102e50983afed9336059593f9a08e5b
Devika <devikasujith12@gmail.com>
2022-05-20 16:39:58 +0400

    Images, header section

---
+++ github-user-search-app/src/components/HeaderSection.vue
@@ -0,0 +1,48 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="header">
+        <h1>devfinder</h1>
+        <div class="color-switch">
+          <span class="color-switch__title">Dark</span>
+          <img class="color-switch__icon" src="../assets/images/icon-moon.svg" alt="">
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+  .header {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+  }
+  h1 {
+    color: var(--clr-black-3);
+    font-size: var(--ff-500);
+  }
+
+  .color-switch {
+    display: flex;
+    align-content: center;
+    justify-content: center;
+    position: relative;
+
+    &__title {
+      text-transform: uppercase;
+      color: var(--clr-grey-blue);
+      font-weight: 700;
+      font-size: var(--ff-100);
+    }
+
+    &__icon {
+      width: 2rem;
+      height: 2rem;
+      position: absolute;
+      left: 4.5rem;
+      bottom: 2rem;
+    }
+  }
+</style>
